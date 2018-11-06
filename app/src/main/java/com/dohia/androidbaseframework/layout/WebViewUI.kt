80559d9230d676036c090c268de2062864a49959
dht <18801147313@163.com>
2018-11-06 14:23:25 +0800

    WebView布局

---
+++ app/src/main/java/com/dohia/androidbaseframework/layout/WebViewUI.kt
@@ -0,0 +1,16 @@
+package com.dohia.androidbaseframework.layout
+
+import com.dohia.androidbaseframework.activity.WebViewActivity
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+
+/**
+Date: 2018/11/6
+Time: 14:19
+author: duhaitao
+ */
+class WebViewUI : AnkoComponent<WebViewActivity> {
+    override fun createView(ui: AnkoContext<WebViewActivity>) = with(ui) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
