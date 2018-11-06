3d1ea10f3dd8e50b6eda04a5efd809e8f7f87d88
dht <18801147313@163.com>
2018-11-06 14:33:58 +0800

    Guide布局

--- app/src/main/java/com/dohia/androidbaseframework/layout/GuideUI.kt
+++ app/src/main/java/com/dohia/androidbaseframework/layout/GuideUI.kt
@@ -1,9 +1,18 @@
 package com.dohia.androidbaseframework.layout
 
+import android.view.View
+import com.dohia.androidbaseframework.activity.GuideActivity
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+
 /**
 Date: 2018/11/6
 Time: 14:25
 author: duhaitao
  */
-class GuideUI {
+class GuideUI : AnkoComponent<GuideActivity>{
+    override fun createView(ui: AnkoContext<GuideActivity>) = with(ui) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
