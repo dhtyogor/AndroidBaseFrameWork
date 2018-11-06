1171679b11fa94d51f25298fb1f5859521840fba
dht <18801147313@163.com>
2018-11-06 14:48:27 +0800

    Guide布局

--- app/src/main/java/com/dohia/androidbaseframework/layout/WebViewUI.kt
+++ app/src/main/java/com/dohia/androidbaseframework/layout/WebViewUI.kt
@@ -3,6 +3,7 @@ package com.dohia.androidbaseframework.layout
 import com.dohia.androidbaseframework.activity.WebViewActivity
 import org.jetbrains.anko.AnkoComponent
 import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.relativeLayout
 
 /**
 Date: 2018/11/6
@@ -11,6 +12,6 @@ author: duhaitao
  */
 class WebViewUI : AnkoComponent<WebViewActivity> {
     override fun createView(ui: AnkoContext<WebViewActivity>) = with(ui) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+       relativeLayout {  }
     }
 }
